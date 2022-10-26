dc48e973c64911701f10b1310df1987dc187e4d7
oscar anillo <oscarani14@gmail.com>
2022-10-26 17:53:39 -0500

    add data folder, avatar component

--- src/Components/avatar-component.js
+++ src/Components/avatar-component.js
@@ -1,7 +1,42 @@
+import { useState } from 'react';
+import testimonials from '../data/data.js';
+
 export default function AvatarComponent(){
+    const [propertiesInfo] = useState(testimonials);
+    const [subject, setSubject] = useState(testimonials[0]);
+
+    const nextTestimonial = () => {
+        const newIndex = subject.index + 1;
+        setSubject(propertiesInfo[newIndex]);
+      };
+    
+      const prevTestimonial = () => {
+        const newIndex = subject.index - 1;
+        setSubject(propertiesInfo[newIndex]);
+      };
+
+      const {id, index, name, avatar, position, testimonial} = subject;
+
     return (
         <div className="bg-avatar"> 
-            <img src="/images/image-tanya.jpg" alt="" className="tanya" />
+            <div key={id}>
+                <div>
+                    <img src={avatar} alt="" className="avatar" />
+                </div>
+                <div className="bg-quotes">
+                    <img src="/images/pattern-quotes.svg" alt="" className="quotes" />
+                    <p className="testimonial">"{testimonial}"</p>
+                    <p className='testimonial-name'><strong>{name}</strong></p>
+                    <p className='testimonial-position'>{position}</p>
+                </div>
+            </div>
+            
+            <button onClick={prevTestimonial} disabled={index === 0}>
+                <img src="/images/icon-prev.svg" alt="" className="prev"/>
+            </button>
+            <button onClick={nextTestimonial} disabled={index === 1}>
+                <img src="/images/icon-next.svg" alt="" className="next" />
+            </button>
         </div>
     )
 }
---
+++ src/data/data.js
@@ -0,0 +1,20 @@
+const testimonials = [
+    {
+        id:1,
+        index:0,
+        name:"Tanya Sinclair",
+        avatar:"/images/image-tanya.jpg",
+        position:"UX Engineer",
+        testimonial:"I've been interested in coding for a while but never taken the jump, until now. I couldn't recommend this course enough. I'm now in the job of my dreams  and so excited about the future."
+    },
+    {
+        id:2,
+        index:1,
+        name:"John Tarkpor",
+        avatar:"/images/image-john.jpg",
+        position:"Junior Front-end Developer",
+        testimonial:"If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer."
+    }
+]
+
+export default testimonials;
